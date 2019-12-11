4eac72044c701f81391f004323b8a5aa60c6495a
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-12-11 16:09:57 +0100

    board working

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit } from "@angular/core";
 export class BoardComponent implements OnInit {
   verifId: number = 0;
   boardID: number;
-  boardX: number[] = [];
-  boardY: number[] = [];
+  boardX: number[];
+  boardY: number[][] = [];
+  boardPlayed: number[][] = [];
+  value: number = 0;
+  random: number = 0;
 
   createBoard(boardTaille: number) {
     if (this.verifId > 0) {
@@ -17,16 +20,40 @@ export class BoardComponent implements OnInit {
     } else {
       this.boardID = 1;
     }
-    for (var i = 1; i <= boardTaille; i++) {
-      this.boardX.push(i);
-      this.boardY.push(i);
+    for (var j = 1; j <= boardTaille; j++) {
+      this.boardX = [];
+      for (var i = 1; i <= boardTaille; i++) {
+        this.getRandomInt(boardTaille);
+        if (this.random > 3) {
+          this.boardX.push(2);
+        } else {
+          this.boardX.push(0);
+        }
+      }
+      this.boardY.push(this.boardX);
     }
+    for (var j = 1; j <= boardTaille; j++) {
+      this.boardX = [];
+      for (var i = 1; i <= boardTaille; i++) {
+        this.boardX.push(-1);
+      }
+      this.boardPlayed.push(this.boardX);
+    }
+  }
+
+  getRandomInt(boardTaille: number) {
+    this.random = Math.floor(Math.random() * Math.floor(5));
+  }
+
+  attack(x: number, y: number) {
+    console.log(y, x);
+
+    this.boardPlayed[y][x] = this.boardY[y][x];
   }
 
   constructor() {}
 
   ngOnInit() {
     this.createBoard(5);
-    console.log("test");
   }
 }
