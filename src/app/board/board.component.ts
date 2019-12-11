9c7ff09938c45f003839f0010a0853a6090ca982
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-12-11 09:07:09 +0100

    Complete basic board component + bootstrap design

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.css"]
 })
 export class BoardComponent implements OnInit {
+  verifId: number = 0;
+  boardID: number;
+  boardX: number[] = [];
+  boardY: number[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  createBoard(boardTaille: number) {
+    if (this.verifId > 0) {
+      this.boardID = 2;
+    } else {
+      this.boardID = 1;
+    }
+    for (var i = 1; i <= boardTaille; i++) {
+      this.boardX.push(i);
+      this.boardY.push(i);
+    }
   }
 
+  constructor() {}
+
+  ngOnInit() {
+    this.createBoard(5);
+    console.log("test");
+  }
 }
